789bdefa261196e9e1f87a2711e51b2af5436603
Tal <tal.yaron@gmail.com>
2023-12-26 18:01:03 +0200

    1.1.58 - bring back unorderd statementModel and imagesURL

--- src/models/statementsModels.ts
+++ src/models/statementsModels.ts
@@ -31,67 +31,62 @@ export const SimpleStatementSchema = z.object({
 export type SimpleStatement = z.infer<typeof SimpleStatementSchema>;
 
 export const StatementSchema = z.object({
-  consensus: z.number(), //depracted
-  createdAt: z.number(),
-  creator: UserSchema,
+  statement: z.string(), //the text of the statement
+  statementId: z.string(),
   creatorId: z.string(),
+  creator: UserSchema,
   defaultLanguage: z.string().length(2).optional(),
-  elementHight: z.number().optional(), // TODO: check if this is needed in the future
-  evaluation: z
-    .object({
-      con: z.number().optional(),
-      fairness: z.number().optional(),
-      pro: z.number().optional(),
-    })
-    .optional(), // TODO: remove this field after removing con, pro and consensus from the statement (20/1/24)
+  parentId: z.string(),
+  parents: z.array(z.string()).optional(), //all parents of the statement, orderd by the hirarchy
+  topParentId: z.string().optional(), //the upper most statement in the hirarchy
   hasChildren: z.boolean().optional(), //should be true if the statement can have children. this lets admin prevent having children.
+  lastMessage: z.string().optional(),
+  lastUpdate: z.number(),
+  lastChildUpdate: z.number().optional(), //keep track of the last child update.
+  createdAt: z.number(),
+  pro: z.number().optional(), //depracted
+  con: z.number().optional(), //depracted
+  evaluation: z.object({
+    pro: z.number().optional(),
+    con: z.number().optional(),
+    fairness: z.number().optional(),
+  }).optional(), // TODO: remove this field after removing con, pro and consensus from the statement (20/1/24)
+  consensus: z.number(), //depracted
+  order: z.number().optional(), // TODO: check if this is needed in the future
+  elementHight: z.number().optional(), // TODO: check if this is needed in the future
+  votes: z.number().optional(), //TODO: remove (probably not needed)
+  selections: z.any().optional(), //TODO: rename to optionsVotes
+  voted: z.number().optional(),  //TODO: remove (probably not needed)
+  totalSubStatements: z.number().optional(), //It is being used to know howm mant statements were not read yet
+  subScreens: z.array(ScreenSchema).optional(), //deprecated TODO: remove after code changing TODO: change code (see room settings  )
+  roomsState: RoomsStateSelectionEnum.optional(), //being for room selection 
+  statementSettings: z.object({
+    subScreens: z.array(ScreenSchema).optional(), //holds the navigation tabs of the statement
+    enableAddEvaluationOption: z.boolean().optional(), //if true, non admin users can add options under evaluation screen
+    enableAddVotingOption: z.boolean().optional(), //if true, non admin users can add options under voting screen
+  }).optional(),
+  maxConsensus: z.number().optional(), //depracted
+  maxConsesusStatement: SimpleStatementSchema.optional(), //TODO: remove (probably not needed)
+  statementType: SimpleStatementTypeSchema.optional(), 
+  resultsSettings: z
+    .object({
+      resultsBy: ResultsBySchema, //top options, top votes, top fairness etc,
+      numberOfResults: z.number().optional(), //how many results will be converted to results
+      deep: z.number().optional(), //how deep the results will go
+      minConsensus: z.number().optional(), //used for fariness cutoff: only fairness score above this number will become results
+    })
+    .optional(),
+  results: z.array(SimpleStatementSchema).optional(),
+  // canHaveChildren: z.boolean().optional(), //deprecated
+  roomSize: z.number().optional(), //deprecated TODO: change code
+  roomsSettings: z.object({   //TODO: change code
+    roomSize: z.number().optional(),
+    roomsState: RoomsStateSelectionEnum.optional(), //being for room selection 
+  }).optional(),
   imagesURL:z.object({
     main:z.string().optional(),
     more:z.array(z.string()).optional()
   }).optional(),
-  lastChildUpdate: z.number().optional(), //keep track of the last child update.
-  lastMessage: z.string().optional(),
-  lastUpdate: z.number(),
-  maxConsesusStatement: SimpleStatementSchema.optional(), //TODO: remove (probably not needed)
-  maxConsensus: z.number().optional(), //depracted
-  order: z.number().optional(), // TODO: check if this is needed in the future
-  parentId: z.string(),
-  parents: z.array(z.string()).optional(), //all parents of the statement, orderd by the hirarchy
-  pro: z.number().optional(), //depracted
-  results: z.array(SimpleStatementSchema).optional(),
-  resultsSettings: z
-    .object({
-      deep: z.number().optional(), //how deep the results will go
-      minConsensus: z.number().optional(), //used for fariness cutoff: only fairness score above this number will become results
-      numberOfResults: z.number().optional(), //how many results will be converted to results
-      resultsBy: ResultsBySchema, //top options, top votes, top fairness etc,
-    })
-    .optional(),
-  roomSize: z.number().optional(), //deprecated TODO: change code
-  roomsSettings: z
-    .object({
-      //TODO: change code
-      roomSize: z.number().optional(),
-      roomsState: RoomsStateSelectionEnum.optional(), //being for room selection
-    })
-    .optional(),
-  roomsState: RoomsStateSelectionEnum.optional(), //being for room selection
-  selections: z.any().optional(), //TODO: rename to optionsVotes
-  statement: z.string(), //the text of the statement
-  statementId: z.string(),
-  statementSettings: z
-    .object({
-      enableAddEvaluationOption: z.boolean().optional(), //if true, non admin users can add options under evaluation screen
-      enableAddVotingOption: z.boolean().optional(), //if true, non admin users can add options under voting screen
-      subScreens: z.array(ScreenSchema).optional(), //holds the navigation tabs of the statement
-    })
-    .optional(),
-  statementType: SimpleStatementTypeSchema.optional(),
-  subScreens: z.array(ScreenSchema).optional(), //deprecated TODO: remove after code changing TODO: change code (see room settings  )
-  totalSubStatements: z.number().optional(), //It is being used to know howm mant statements were not read yet
-  topParentId: z.string(),
-  votes: z.number().optional(), //TODO: remove (probably not needed)
-  voted: z.number().optional(), //TODO: remove (probably not needed)
 });
 
 export type Statement = z.infer<typeof StatementSchema>;
