f2656811e8bc5217c9eb91009b924cba2ea90b10
Tal Yaron <tal.yaron@gmail.com>
2024-10-07 17:23:53 +0300

    cont

--- src/controllers/statementsCont.ts
+++ src/controllers/statementsCont.ts
@@ -9,8 +9,8 @@ export function statementToSimpleStatement(statement: Statement): SimpleStatemen
         creatorId: statement.creatorId,
         creator: statement.creator,
         parentId: statement.parentId,
-        parents: statement.parents || [],
-        topParentId: statement.topParentId,
+        consensus: statement.consensus,
+        voted: statement.voted
     };
 
     //remove properties that are undefined
