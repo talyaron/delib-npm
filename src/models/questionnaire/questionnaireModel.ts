ca9a37436816d21bee3b9d112838fe3f67dc16c1
Tal Yaron <tal.yaron@gmail.com>
2025-08-17 09:15:11 +0300

    5.6.27

--- src/models/questionnaire/questionnaireModel.ts
+++ src/models/questionnaire/questionnaireModel.ts
@@ -7,26 +7,17 @@ import { CutoffBy } from "../results/Results";
 export const QuestionnaireQuestionSchema = object({
     questionnaireId: string(), // unique identifier for the questionnaire
     statementId: string(), // unique identifier for the statement that the question uses to store data
-    questionType: optional(enum_(QuestionType)),
     evaluationUI: optional(enum_(EvaluationUI)),
-    cutoffBy: optional(enum_(CutoffBy)),
-    sorting: optional(enum_(SortType)),
-    number: optional(number()), // optional number to cut off by
-    image: optional(string()), // optional image URL for the question
-    questionnaireQuestionId: string(), // unique identifier for the questionnaire question
     order: number(), // order of the question in the questionnaire
-}); 
+});
 
 export type QuestionnaireQuestion = InferOutput<typeof QuestionnaireQuestionSchema>;
 
 export const QuestionnaireSchema = object({
     questionnaireId: string(), // unique identifier for the questionnaire
-    statement: string(), // the title of the questionnaire
-    description: optional(string()), // optional description of the questionnaire
-    image: optional(string()), // optional image URL for the questionnaire
     createdAt: optional(number()), // timestamp of when the questionnaire was created
     updatedAt: optional(number()), // timestamp of the last update to the questionnaire
-    questions: record(string(), QuestionnaireQuestionSchema), // map of questionnaireQuestionId -> QuestionnaireQuestion
+    questions: array(QuestionnaireQuestionSchema), // array of QuestionnaireQuestion
 });
 
 export type Questionnaire = InferOutput<typeof QuestionnaireSchema>;
